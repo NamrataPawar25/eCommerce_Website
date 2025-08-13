159d7d42845b5966bcb7704aca1c2fdc2c07f8a6
NamrataPawar25 <namratapawar9793@gmail.com>
2025-08-13 12:03:33 +0530

    Card

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,22 +1,28 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
-import products from '../data'
+import React, { useContext } from 'react';
+import { Link } from 'react-router-dom';
+import { CreateTheme } from "../context/ThemeProvider";
 
-const Card = ({product}) => {
-    return (
-        <div>
-            <div className="card" style={{width: "18rem"}}>
-                <img src={product.image} className="card-img-top" alt="..." />
-                    <div className="card-body">
-                        <h5 className="card-title">{product.name}</h5>
-                        <p className="card-text">{product.description}</p>
-                        {product.discount ?(<p>Price: ₹<del>{product.price}</del><span className='ps-2'>{product.discountedPrice}</span></p>):(<p>Price: ₹{product.price}</p>)}
-                        <Link to= {`/dashboard/${product.id}`} className="btn btn-primary">View More</Link>
-                        <button className='btn btn-secondary ms-3'>Add to cart</button>
-                    </div>
-            </div>
+const Card = ({ product }) => {
+  const { theme } = useContext(CreateTheme);
+
+  return (
+    <div>
+      <div className={`card d-flex flex-column my-3 ${theme === 'dark' ? 'bg-dark text-light' : 'bg-white text-dark'}`} style={{ width: '18rem' } }>
+        <img src={product.image} className="card-img-top" alt={product.name} />
+        <div className="card-body">
+          <h5 className="card-title">{product.name}</h5>
+          <p className="card-text">{product.description}</p>
+          {product.discount ? (
+            <p>Price: ₹<del>{product.price}</del><span className='ps-2'>{product.discountedPrice}</span></p>
+          ) : (
+            <p>Price: ₹{product.price}</p>
+          )}
+          <Link to={`/dashboard/${product.id}`} className="btn btn-primary">View More</Link>
+          <button className='btn btn-secondary ms-3'>Add to cart</button>
         </div>
-    )
-}
+      </div>
+    </div>
+  );
+};
 
-export default Card
+export default Card;
