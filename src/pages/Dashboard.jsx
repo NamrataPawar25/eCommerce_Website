7887c0088006cbc12231dfa178a7242ebbad4930
NamrataPawar25 <namratapawar9793@gmail.com>
2025-08-13 12:03:44 +0530

    Dashboard

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -1,12 +1,15 @@
 import React, { useState } from 'react'
 import Card from '../components/Card'
+import { CreateTheme } from "../context/ThemeProvider";
+import { useContext } from 'react';
 
 
 const Dashboard = ({ products }) => {
     const [productstate, setProductstate] = useState(products)
+      const { theme } = useContext(CreateTheme)
 
     return (
-        <>
+        <div className={`${theme === "light" ? "bg-light text-dark" : "bg-dark text-light"}`}>
         <div className="container d-flex flex-wrap">
             {productstate.map((product, index) => (
                     <div className='badge bg-secondary mx-3 my-3 p-3' key={index}>
@@ -23,7 +26,7 @@ const Dashboard = ({ products }) => {
                 ))}
             </div>
         </div>
-        </>
+        </div>
     )
 }
 
